c891fe3438773adba9e71789a6e8786d9c47d143
TarasDer <114342061+TarasDer@users.noreply.github.com>
2023-04-01 19:59:56 +0000

    Deploying to gh-pages from @ TarasDer/goit-js-hw-09@1a3d0e78b6be5c67da0a4d5e531be5f79de4b327 🚀

--- 01-color-switcher.4fbac47e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(()=>{o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.4fbac47e.js.map
---
+++ 01-color-switcher.99d92486.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{e.removeAttribute("disabled"),d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),clearInterval(d),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.99d92486.js.map
